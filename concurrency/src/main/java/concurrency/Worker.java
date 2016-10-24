3d9884247111420973304d00bc58096f2ebfcb58
ASaunin <alsaunin@gmail.com>
2016-10-24 11:14:46 +0300

    Add simple Worker implementation

---
+++ concurrency/src/main/java/concurrency/Worker.java
@@ -0,0 +1,50 @@
+package concurrency;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Worker {
+
+    private final Queue<Runnable> tasks = new LinkedList<>();
+
+    private class Runner implements Runnable {
+        @Override
+        public void run() {
+            Runnable task;
+            while (true) {
+                synchronized (tasks) {
+                    while (tasks.isEmpty()) {
+                        try {
+                            tasks.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    task = tasks.poll();
+                }
+                task.run();
+            }
+        }
+    }
+
+    public Worker() {
+        new Thread(new Runner()).start();
+    }
+
+    public void execute(Runnable task) {
+        synchronized (tasks) {
+            tasks.offer(task);
+            tasks.notify();
+        }
+    }
+
+    public static void main(String[] args) {
+        final Worker worker = new Worker();
+        for (int i = 0; i < 10; i++) {
+            final int finalI = i;
+            final Runnable task = () -> System.out.printf("Thread %d execute\n", finalI);
+            worker.execute(task);
+        }
+    }
+
+}
