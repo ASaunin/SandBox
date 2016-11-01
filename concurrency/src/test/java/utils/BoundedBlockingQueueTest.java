93f898899e907019f46e957d95eb4cb438286ade
ASaunin <alsaunin@gmail.com>
2016-11-01 23:17:44 +0300

    Add bounded blocking queue class and test

---
+++ concurrency/src/test/java/utils/BoundedBlockingQueueTest.java
@@ -0,0 +1,47 @@
+package utils;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runners.model.TestTimedOutException;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class BoundedBlockingQueueTest {
+
+    private static final int MAX_CAPACITY = 2;
+    private BoundedBlockingQueue<Integer> queue = new BoundedBlockingQueue<>(MAX_CAPACITY);
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test(timeout = 100)
+    public void takingItemFromEmptyQueueFailsByTimeout() throws Exception {
+        thrown.expect(TestTimedOutException.class);
+        queue.take();
+    }
+
+    @Test(timeout = 100)
+    public void puttingItemToFullQueueFailsByTimeout() throws Exception {
+        for (int i = 0; i < MAX_CAPACITY; i++) {
+            queue.put(i);
+        }
+        thrown.expect(TestTimedOutException.class);
+        queue.put(MAX_CAPACITY);
+    }
+
+    @Test
+    public void putAndTakenItemsAreEqual() throws Exception {
+        final int expectedValue = ThreadLocalRandom.current().nextInt();
+        queue.put(expectedValue);
+
+        assertThat(queue.take(), is(expectedValue));
+    }
+
+
+
+
+}
