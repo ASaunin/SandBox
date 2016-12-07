f731d1980f5745c086398b820155ac4355cdc6f6
ASaunin <alsaunin@gmail.com>
2016-12-07 09:30:07 +0300

    Custom thread pool manager (not finished)

--- concurrency/src/main/java/executors/ThreadPoolManager.java
+++ concurrency/src/main/java/executors/ThreadPoolManager.java
@@ -1,43 +1,63 @@
 package executors;
 
-import utils.ThreadUtils;
-
-import java.util.List;
 import java.util.concurrent.*;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-import java.util.stream.IntStream;
-
-import static java.util.stream.Collectors.toList;
 
 public class ThreadPoolManager {
 
     private static final ThreadPoolManager INSTANCE = new ThreadPoolManager();
     private static final ExecutorService executor = newFixedThreadPoolWithArrayBlockingQueue();
-    private final Semaphore writeLock = new Semaphore(0);
-    private final Semaphore readLock = new Semaphore(0);
-
-    private static int readersCount = 0;
+    private static final SubscriberQueue readerQueue = new SubscriberQueue();
+    private static final SubscriberQueue writerQueue = new SubscriberQueue();
+//    private static final LinkedBlockingQueue<Runnable> bufferQueue = new LinkedBlockingQueue<>();
 
     public static ThreadPoolManager getInstance() {
         return INSTANCE;
     }
 
-    public void submitWriteTask(Task task) {
-        System.out.printf("%s requests for task %s\n", Thread.currentThread(), task.getName());
+    public void submitWriteTask(Runnable task) {
+        System.out.printf("%s requests to write\n", task);
+        if (readerQueue.hasItems() || writerQueue.hasItems()) {
+//            bufferQueue.offer(task);
+            writerQueue.put();
+            System.out.printf("%s locked\n", task);
+            writerQueue.lock();
+            System.out.printf("%s unlocked\n", task);
+        } else {
+            writerQueue.put();
+        }
         CompletableFuture.runAsync(task, executor)
                 .whenComplete((value, exception) -> {
+                    writerQueue.get();
+                    if (writerQueue.hasItems()) {
+                        writerQueue.unlock(); //Release single write tasks (could start simultaneously)
+                    } else {
+                        readerQueue.unlockAll(); //Release all read tasks (could start in parallel)
+                    }
+
                 });
     }
 
-    public void submitReadTask(Task task) {
-        System.out.printf("%s requests for task %s\n", Thread.currentThread(), task.getName());
+    public void submitReadTask(Runnable task) {
+        System.out.printf("%s requests to read\n", task);
+        if (writerQueue.hasItems()) {
+//            bufferQueue.offer(task);
+            readerQueue.put();
+            System.out.printf("%s locked\n", task);
+            readerQueue.lock();
+            System.out.printf("%s unlocked\n", task);
+        } else {
+            readerQueue.put();
+        }
         CompletableFuture.runAsync(task, executor)
                 .whenComplete((value, exception) -> {
+                    readerQueue.get();
+                    if (readerQueue.isEmpty()) {
+                        writerQueue.unlock(); //Release single write tasks (could start simultaneously)
+                    }
                 });
     }
 
-    private static ThreadPoolExecutor newFixedThreadPoolWithArrayBlockingQueue() {
+    public static ThreadPoolExecutor newFixedThreadPoolWithArrayBlockingQueue() {
         final int corePoolSize = 10;
         final int maxPoolSize = 20;
         final long keepAliveTime = 10;
@@ -46,61 +66,109 @@ public class ThreadPoolManager {
         return new ThreadPoolExecutor(corePoolSize, maxPoolSize, keepAliveTime, TimeUnit.MINUTES, new ArrayBlockingQueue<>(queueSize));
     }
 
-    public static void main(String[] args) {
-        final int N_TASKS = 20;
-        final int MIN_TIMEOUT = 1000;
-        final int MAX_TIMEOUT = 20000;
+//    public static void main(String[] args) {
+//        final int N_TASKS = 20;
+//        final int MIN_TIMEOUT = 1000;
+//        final int MAX_TIMEOUT = 5000;
+//
+//        List<Task> readTasks = IntStream.range(0, N_TASKS)
+//                .mapToObj((i) -> new Task(MIN_TIMEOUT, MAX_TIMEOUT, "Read" + i))
+//                .limit(N_TASKS)
+//                .collect(toList());
+//
+//        List<Task> writeTasks = IntStream.range(0, N_TASKS)
+//                .mapToObj((i) -> new Task(MIN_TIMEOUT, MAX_TIMEOUT, "Write" + i))
+//                .limit(N_TASKS)
+//                .collect(toList());
+//
+//        ThreadPoolManager manager = getInstance();
+//
+//        manager.submitReadTask(readTasks.get(0));
+//        manager.submitReadTask(readTasks.get(1));
+//        manager.submitWriteTask(writeTasks.get(0));
+//        manager.submitReadTask(readTasks.get(2));
+//        manager.submitReadTask(readTasks.get(3));
+//        manager.submitReadTask(readTasks.get(4));
+//        manager.submitWriteTask(writeTasks.get(2));
+//        manager.submitWriteTask(writeTasks.get(3));
+//        manager.submitReadTask(readTasks.get(5));
+//        manager.submitReadTask(readTasks.get(6));
+//
+//        executor.shutdown();
+//
+//    }
+//
+//    private static class Task implements Runnable {
+//
+//        private String name;
+//        private long minMillis;
+//        private long maxMillis;
+//
+//        private Task(long minMillis, long maxMillis, String name) {
+//            this.minMillis = minMillis;
+//            this.maxMillis = maxMillis;
+//            this.name = name;
+//        }
+//
+//        @Override
+//        public void run() {
+//            final Thread currentThread = Thread.currentThread();
+//            currentThread.setName(name);
+//            System.out.printf("%s started work\n", name);
+//            ThreadUtils.randomSleep(minMillis, maxMillis);
+//            System.out.printf("%s finished work\n", name);
+//            if (name.equals("Read3"))
+//                throw new UnsupportedOperationException();
+//        }
+//
+//        public String getName() {
+//            return name;
+//        }
+//
+//    }
 
-        List<Task> readTasks = IntStream.range(0, N_TASKS)
-                .mapToObj((i) -> new Task(MIN_TIMEOUT, MAX_TIMEOUT, "Read" + i))
-                .limit(N_TASKS)
-                .collect(toList());
+    private static class SubscriberQueue {
 
-        List<Task> writeTasks = IntStream.range(0, N_TASKS)
-                .mapToObj((i) -> new Task(MIN_TIMEOUT, MAX_TIMEOUT, "Write" + i))
-                .limit(N_TASKS)
-                .collect(toList());
+        private static final Semaphore semaphore = new Semaphore(0);
+        private int count = 0;
 
-        ThreadPoolManager manager = getInstance();
-
-        manager.submitReadTask(readTasks.get(0));
-        ThreadUtils.sleep(10);
-        manager.submitReadTask(readTasks.get(1));
-        ThreadUtils.sleep(10);
-        manager.submitWriteTask(writeTasks.get(0));
-        ThreadUtils.sleep(10);
-        manager.submitReadTask(readTasks.get(2));
-        ThreadUtils.sleep(10);
-
-        executor.shutdown();
-
-    }
-
-    static class Task implements Runnable {
-
-        private String name;
-        private long minMillis;
-        private long maxMillis;
-
-        private Task(long minMillis, long maxMillis, String name) {
-            this.minMillis = minMillis;
-            this.maxMillis = maxMillis;
-            this.name = name;
+        public int getCount() {
+            return count;
         }
 
-        @Override
-        public void run() {
-            final Thread currentThread = Thread.currentThread();
-            currentThread.setName(name);
-            System.out.printf("Thread %s started work\n", name);
-            ThreadUtils.randomSleep(minMillis, maxMillis);
-            System.out.printf("Thread %s finished work\n", name);
-
+        public boolean isEmpty() {
+            return count == 0;
         }
 
-        public String getName() {
-            return name;
+        public boolean hasItems() {
+            return count != 0;
         }
+
+        public void lock() {
+            semaphore.acquireUninterruptibly();
+        }
+
+        public void lockAll() {
+            semaphore.acquireUninterruptibly(count);
+        }
+
+        public void unlock() {
+            if (count > 0)
+                semaphore.release();
+        }
+
+        public void unlockAll() {
+            semaphore.release(count);
+        }
+
+        public void put() {
+            count++;
+        }
+
+        public void get() {
+            count--;
+        }
+
     }
 
 }
---
+++ concurrency/src/main/java/executors/ThreadPoolManagerApp.java
@@ -0,0 +1,64 @@
+package executors;
+
+import utils.ThreadUtils;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.IntStream;
+
+public class ThreadPoolManagerApp {
+
+    private static class Task implements Runnable {
+
+        private String name;
+        private long minMillis;
+        private long maxMillis;
+
+        private Task(long minMillis, long maxMillis, String name) {
+            this.minMillis = minMillis;
+            this.maxMillis = maxMillis;
+            this.name = name;
+        }
+
+        @Override
+        public void run() {
+            final Thread currentThread = Thread.currentThread();
+            currentThread.setName(name);
+            System.out.printf("%s started work\n", name);
+            ThreadUtils.randomSleep(minMillis, maxMillis);
+            System.out.printf("%s finished work\n", name);
+//            if (name.equals("Read3"))
+//                throw new UnsupportedOperationException();
+        }
+
+        public String getName() {
+            return name;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        final ThreadPoolManager manager = ThreadPoolManager.getInstance();
+        final ExecutorService executor = Executors.newFixedThreadPool(20);
+        final int N_TASKS = 10;
+        final int MIN_TIMEOUT = 1000;
+        final int MAX_TIMEOUT = 5000;
+
+        IntStream.range(0, N_TASKS)
+                .mapToObj((i) ->
+                        (Runnable) () -> {
+                            if (ThreadLocalRandom.current().nextInt(2) == 0)
+                                manager.submitReadTask(new Task(MIN_TIMEOUT, MAX_TIMEOUT, "Read" + i));
+                            else
+                                manager.submitWriteTask(new Task(MIN_TIMEOUT, MAX_TIMEOUT, "Write" + i));
+                        }
+                )
+                .limit(N_TASKS)
+                .forEach(executor::execute);
+
+        executor.shutdown();
+
+    }
+
+}
